336563d116a94ef985d005aea8165ee4b71b5505
AD001\z003t7kt <ercan.konus@siemens.com>
2018-09-12 00:09:15 +0300

    Revert "Initial commit"
    This reverts commit b0a049b208d276e8da68d0d266bce55e6bc948f2.

--- src/main/java/com/example/fourdsight/communication/message/ImageMessage.java
+++
@@ -1,8 +0,0 @@
-package com.example.fourdsight.communication.message;
-
-import lombok.Data;
-
-@Data
-public class ImageMessage {
-    private String url;
-}
--- src/main/java/com/example/fourdsight/util/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.fourdsight.util;
-
-public final class Constants {
-
-    public static final int URL_MAX_LENGTH = 200;
-}
